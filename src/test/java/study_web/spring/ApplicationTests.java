d037fdbf77b7f3b43fb60e64f9340c35b163be1c
SUNGMIN\sungmin <lion.h2ad@gmail.com>
2025-03-25 22:21:03 +0900

    init commit

---
+++ src/test/java/study_web/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package study_web.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
