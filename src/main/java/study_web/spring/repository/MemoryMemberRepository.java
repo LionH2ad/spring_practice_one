c48546c31c8b3ab3e0b19307752f5083a00d7ea2
SUNGMIN\sungmin <lion.h2ad@gmail.com>
2025-03-26 15:10:21 +0900

    test case update

--- src/main/java/study_web/spring/repository/MemoryMemberRepository.java
+++ src/main/java/study_web/spring/repository/MemoryMemberRepository.java
@@ -32,4 +32,8 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> findAll() {
         return new ArrayList<Member>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
---
+++ src/main/java/study_web/spring/service/MemberService.java
@@ -0,0 +1,46 @@
+package study_web.spring.service;
+
+import study_web.spring.domain.Member;
+import study_web.spring.repository.MemberRepository;
+import study_web.spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        /*Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(member1 -> {
+            throw new IllegalStateException("Member already exists");
+        });*/
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(member1 -> {
+                    throw new IllegalStateException("Member already exists");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * */
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
